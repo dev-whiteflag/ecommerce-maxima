8e2f951b6fc5c273313988e0f9fbfa883d2595a1
Brenno Fagundes <dev.whiteflag@gmail.com>
2020-10-16 12:10:41 -0300

    feat: added config server, eureka server (discovery)

---
+++ backend/discovery/src/main/java/com/maximatech/ecommerce/discovery/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.maximatech.ecommerce.discovery.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${maxima.discovery.user}")
+    private String user;
+
+    @Value("${maxima.discovery.password}")
+    private String password;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(user)
+                .password(password)
+                .roles("SYSTEM");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and().requestMatchers().antMatchers("/eureka/**")
+                .and().authorizeRequests().antMatchers("/eureka/**").hasRole("SYSTEM").anyRequest().denyAll()
+                .and().httpBasic()
+                .and().csrf().disable();
+    }
+}
