b9ed2b4c4ed57521dbd82acd55941ff432bffff5
Brenno Fagundes <dev.whiteflag@gmail.com>
2020-10-18 13:11:55 -0300

    feat: added gateway config

--- backend/gateway/src/main/java/com/maximatech/ecommerce/gateway/config/SecurityConfig.java
+++ backend/gateway/src/main/java/com/maximatech/ecommerce/gateway/config/SecurityConfig.java
@@ -1,4 +1,29 @@
 package com.maximatech.ecommerce.gateway.config;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER")
+                .and().withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/eureka/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and().formLogin().and().logout().permitAll()
+                .and().csrf().disable();
+    }
 }
