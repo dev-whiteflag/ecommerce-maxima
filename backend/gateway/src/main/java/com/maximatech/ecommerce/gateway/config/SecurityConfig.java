03bfc89ad34bf86e1f573cabcf3312fc0ddff081
Brenno Fagundes <dev.whiteflag@gmail.com>
2020-10-21 11:26:50 -0300

    feat: cors fix and client service is now working with backend

--- backend/gateway/src/main/java/com/maximatech/ecommerce/gateway/config/SecurityConfig.java
+++ backend/gateway/src/main/java/com/maximatech/ecommerce/gateway/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.maximatech.ecommerce.gateway.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -19,6 +23,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll()
-                .and().csrf().disable();
+                .and().cors().and().csrf().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",
+                new CorsConfiguration().applyPermitDefaultValues()
+        );
+        return source;
     }
 }
